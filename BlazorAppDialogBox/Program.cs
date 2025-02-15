d5e95e8d6bd3c5f7563db7cef07b4ecb77ef3132
amar-tatwa <amarjyoti.mahanta@tatwa.info>
2025-02-15 11:15:54 +0530

    count value pass

--- BlazorAppDialogBox/Program.cs
+++ BlazorAppDialogBox/Program.cs
@@ -1,11 +1,14 @@
 using BlazorAppDialogBox;
+using BlazorAppDialogBox.Pages.DailogComponents;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddScoped<CustomDialogService>();
+builder.Services.AddRadzenComponents();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
